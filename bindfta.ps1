cafdd9430d2a08119884a6e2d32b135ac70487ec
Alonso Rodriguez <alonsorodi20@gmail.com>
2024-01-29 17:36:23 +0100

    Update bindfta.ps1

--- bindfta.ps1
+++ bindfta.ps1
@@ -1,4 +1,4 @@
 $configs = ".\configs\fta"
 $bindings = "files_assoc_nobrowser.txt"
 
-sudo powershell -c "SetUserFTA.exe $configs\$bindings"
+sudo powershell -c "$configs\SetUserFTA.exe $configs\$bindings"
