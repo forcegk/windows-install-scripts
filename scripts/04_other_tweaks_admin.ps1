9033b0f0d9f48d95e3fc623da95553d9db5f425b
Alonso Rodriguez <alonsorodi20@gmail.com>
2024-08-10 16:45:08 +0200

    Update 04_other_tweaks_admin.ps1

--- scripts/04_other_tweaks_admin.ps1
+++ scripts/04_other_tweaks_admin.ps1
@@ -1,6 +1,3 @@
-# Activate Windows (only run once)
-if ((Get-Command "scoop" -ErrorAction SilentlyContinue) -eq $null)
-{
-	slmgr /skms kms.digiboy.ir
-	slmgr /ato
-}
+# Activate Windows
+slmgr /skms kms.digiboy.ir
+slmgr /ato
