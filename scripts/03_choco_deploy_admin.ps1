f997a4cdb07ede791c15b536ba50d0393aaa0ffd
Alonso Rodriguez <alonsorodi20@gmail.com>
2026-02-07 00:55:40 +0100

    Combine multiple choco install commands into one

--- scripts/03_choco_deploy_admin.ps1
+++ scripts/03_choco_deploy_admin.ps1
@@ -4,6 +4,4 @@ if ((Get-Command "choco" -ErrorAction SilentlyContinue) -eq $null)
    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-choco install -y md5
-choco install -y googleearthpro
-choco install -y ImDisk-Toolkit
+choco install -y md5 googleearthpro ImDisk-Toolkit directx-sdk vcredist-all
