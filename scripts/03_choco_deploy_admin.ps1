e148cc365e3cde497ae2fb2724d91a8e645c7b43
Alonso Rodriguez <alonsorodi20@gmail.com>
2026-02-07 01:16:07 +0100

    Add choco-cleaner to installation script

--- scripts/03_choco_deploy_admin.ps1
+++ scripts/03_choco_deploy_admin.ps1
@@ -4,4 +4,4 @@ if ((Get-Command "choco" -ErrorAction SilentlyContinue) -eq $null)
    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-choco install -y md5 googleearthpro ImDisk-Toolkit directx-sdk vcredist-all
+choco install -y md5 googleearthpro ImDisk-Toolkit directx-sdk vcredist-all choco-cleaner
